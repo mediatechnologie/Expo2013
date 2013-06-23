93a05fbfea544ea96c68060cfc7f7a1da5104256
Alex van Andel <alexvanandel@mthub.org>
2013-06-23 21:07:59 +0200

    Cleanup and Tiny css changes

--- assets/scripts/project.js
+++ assets/scripts/project.js
@@ -1,25 +1,5 @@
 (function(window, document, undefined) {
 
-	function previousProject() {
-
-		var index = projects.indexOf(current);
-		if(--index < 0) {
-			index = projects.length - 1;
-		}
-
-		window.location = '../' + projects[index] + '/';
-	}
-
-	function nextProject() {
-
-		var index = projects.indexOf(current);
-		if(++index > projects.length - 1) {
-			index = 0;
-		}
-
-		window.location = '../' + projects[index] + '/';
-	}
-
 	window.onload = function() {
 		var more_btn  = document.getElementById('toggle-more');
 		more_btn.href = (location.hash == '#more') ? '#' : '#more';
@@ -29,17 +9,5 @@
 		var more_btn  = document.getElementById('toggle-more');
 		more_btn.href = (location.hash == '#more') ? '#' : '#more';
 	}
-
-	window.onkeydown = function(evt) {
-	    evt = evt || window.event;
-	    switch (evt.keyCode) {
-	        case 37:
-	            previousProject();
-	            break;
-	        case 39:
-	            nextProject();
-	            break;
-	    }
-	};
 
 })(window, document);
